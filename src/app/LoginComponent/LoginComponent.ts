eb81f4ef1eb2420a1cd460cb3274cee5cc38c21b
Anthony Scinocco <scinocco.a@gmail.com>
2017-02-28 15:42:44 -0500

    building out login form

--- src/app/LoginComponent/LoginComponent.ts
+++ src/app/LoginComponent/LoginComponent.ts
@@ -9,9 +9,15 @@ import { User } from '../Models/User';
 
 export class LoginComponent
 {
+  public title: string;
+  public email: string;
+  public password: string;
+
   constructor()
   {
-
+    this.title = "Login";
+    this.email = "";
+    this.password = "";
   }
 
   public userIsLoggedIn(): boolean
