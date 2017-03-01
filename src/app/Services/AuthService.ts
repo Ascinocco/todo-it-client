70661e8721b975d7a8906aafd6376a906b6aea14
Anthony Scinocco <scinocco.a@gmail.com>
2017-03-01 15:24:55 -0500

    not sure why this is green. Maybe I missed a commit. Just scaffolded basic auth service
    added cli to dependencies as well

--- src/app/Services/AuthService.ts
+++ src/app/Services/AuthService.ts
@@ -1,8 +1,29 @@
 import { Injectable } from '@angular/core';
+import { Http, Headers, Response, Request } from '@angular/http';
 import { User } from '../Models/User';
 
 @Injectable()
 export class AuthService
 {
+    public token: string;
+    private http: Http;
 
+    constructor(http: Http)
+    {
+        let user = new User('anthony@mail.com'); // should be getting user from 
+        // cookie or local storage
+        this.token = "myToken1234"
+    }
+
+    public login()
+    {
+        // return user
+        return new User('scinoccoAnthony@mail.com');
+    }
+
+    public logout()
+    {
+        // destory local storage etc
+        return true
+    }
 }
