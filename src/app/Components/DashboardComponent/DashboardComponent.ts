3996c5244a544ae2ab653b10a499f2504b7a2b1b
Anthony Scinocco <scinocco.a@gmail.com>
2017-03-05 16:18:05 -0500

    Some basic stuff working
    for whatever reason I cannot get a dropdown to work :/

--- src/app/Components/DashboardComponent/DashboardComponent.ts
+++ src/app/Components/DashboardComponent/DashboardComponent.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { LocalStorageService } from 'angular-2-local-storage';
 
 @Component({
     selector: 'dashbaord',
@@ -8,4 +9,20 @@ import { Component } from '@angular/core';
 
 export class DashboardComponent 
 {
+
+    private localStorage: LocalStorageService;
+
+    constructor(localStorage: LocalStorageService)
+    {
+        this.localStorage = localStorage;
+    }
+
+    public getUserFirstName(): string
+    {
+        let user = this.localStorage.get('user');
+
+        if ("firstName" in user) {
+        return user["firstName"];
+        }
+    }
 }
